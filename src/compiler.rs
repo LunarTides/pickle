497bfcd998b9f340adc284f01ea642300d91bbf7
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-09-29 22:38:13 +0200

    Something? Idk im tired

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,13 +1,13 @@
-use crate::parser::ExitNode;
+use crate::parser::NodeExit;
 
-pub fn compile(root: ExitNode) -> String {
+pub fn compile(root: NodeExit) -> String {
     let mut buffer = String::new();
 
     buffer.push_str("global _start\n_start:\n");
 
     buffer.push_str("    mov rax, 60\n");
-    buffer.push_str(format!("    mov rdi, {}\n", root.expr.number.value).as_str());
-    buffer.push_str("syscall\n");
+    buffer.push_str(format!("    mov rdi, {}\n", root.expr.token.value).as_str());
+    buffer.push_str("    syscall\n");
 
     buffer
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,13 @@
 use crate::lexer::{Token, TokenType};
 
 #[derive(Default)]
-pub struct ExprNode {
-    // TODO: Change this
-    pub number: Token,
+pub struct NodeExpr {
+    pub token: Token
 }
 
 #[derive(Default)]
-pub struct ExitNode {
-    pub expr: ExprNode,
+pub struct NodeExit {
+    pub expr: NodeExpr,
 }
 
 #[derive(Default)]
@@ -18,32 +17,24 @@ impl Parser {
     fn parse_expr(
         &self,
         iter: &mut std::vec::IntoIter<Token>,
-        current_token: &Token,
-    ) -> Option<ExprNode> {
+    ) -> NodeExpr {
         let next_token = iter.next().expect("Expected token after expression!");
 
-        if current_token.token_type == TokenType::Keyword && current_token.value == "exit" {
-            if next_token.token_type == TokenType::Number {
-                return Some(ExprNode { number: next_token });
-            } else {
-                panic!("Expected number in `exit`");
-            }
+        if next_token.token_type == TokenType::Number || next_token.token_type == TokenType::Identifer {
+            return NodeExpr { token: next_token };
+        } else {
+            panic!("Expected valid number in expression!");
         }
-
-        None
     }
 
-    pub fn parse(&self, tokens: Vec<Token>) -> ExitNode {
-        let mut exit_node = ExitNode::default();
+    pub fn parse(&self, tokens: Vec<Token>) -> NodeExit {
+        let mut exit_node = NodeExit::default();
 
         let mut iter = tokens.into_iter();
         while let Some(token) = iter.next() {
             if token.token_type == TokenType::Keyword && token.value == "exit" {
-                if let Some(expr_node) = self.parse_expr(&mut iter, &token) {
-                    exit_node.expr = expr_node;
-                } else {
-                    panic!("Invalid expression!");
-                }
+                let expr_node = self.parse_expr(&mut iter);
+                exit_node.expr = expr_node;
             }
         }
 
